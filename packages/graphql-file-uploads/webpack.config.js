66a22a8dc29f66a59f20505e269e1a64493c3756
simon-verhoeven <simonaverhoeven@gmail.com>
2021-08-12 15:09:20 +0800

    added start:ts:node example

--- packages/graphql-file-uploads/webpack.config.js
+++ packages/graphql-file-uploads/webpack.config.js
@@ -1,7 +1,67 @@
-const { merge } = require('webpack-merge');
+/* eslint-disable class-methods-use-this */
 const path = require('path');
-const rootConfig = require(path.join(__dirname, './webpack.config.build.js'));
+const fs = require('fs');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
-const output = merge(rootConfig(__dirname));
+const mode = process.env.NODE_ENV || 'development';
 
-module.exports = output;
+module.exports = () => {
+  const SOURCE_DIR = path.join(__dirname, './src/');
+  const TARGET_DIR = path.join(__dirname, './dist/');
+
+  const filename = 'index.js';
+  const entry = path.join(SOURCE_DIR, 'index.ts');
+
+  const plugins = [];
+
+  return {
+    node: {
+      global: false,
+      __filename: false,
+      __dirname: false,
+    },
+    mode,
+    devtool: 'source-map',
+    entry,
+    target: 'node',
+    resolve: {
+      extensions: ['.mjs', '.ts', '.js'],
+      plugins: [
+        new TsconfigPathsPlugin({
+          /* options: see below */
+        }),
+      ],
+    },
+    output: {
+      libraryTarget: 'commonjs2',
+      path: TARGET_DIR,
+      filename,
+    },
+    module: {
+      rules: [
+        {
+          type: 'javascript/auto',
+          test: /\.mjs$/,
+          // resolve: {
+          //   fullySpecified: false,
+          // },
+        },
+        {
+          test: /.tsx?$/,
+          use: [
+            {
+              loader: 'ts-loader',
+              options: {
+                configFile: 'tsconfig.json',
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins,
+    optimization: {
+      minimize: false,
+    },
+  };
+};
