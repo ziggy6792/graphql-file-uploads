372364dd390b0b2049c226cded12276e830c86f1
simon-verhoeven <simonaverhoeven@gmail.com>
2021-08-12 21:04:49 +0800

    cleanup

--- test/addProfilePicture.sh
+++ test/addProfilePicture.sh
@@ -1,4 +1,5 @@
 curl --location --request POST 'http://localhost:4000/graphql' \
   --form 'operations="{\"query\":\"mutation addProfilePicture($picture: Upload!){\\n  addProfilePicture(picture: $picture)\\n}\"}"' \
   --form 'map="{ \"0\" : [\"variables.picture\"]}"' \
-  --form '0=@"picture.jpeg"'
+  --form '0=@"picture.jpeg"' &&
+  echo "success"
