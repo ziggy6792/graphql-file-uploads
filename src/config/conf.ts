9ca12594ea8eb4b62ade021d1cc1f2d39d21ef6a
simon-verhoeven <simonaverhoeven@gmail.com>
2021-08-17 02:46:04 +0800

    cleanup

---
+++ src/config/conf.ts
@@ -0,0 +1,4 @@
+/* eslint-disable import/prefer-default-export */
+import path from 'path';
+
+export const ROOT_DIR = path.join(__dirname, '../');
--- src/modules/user/ProfilePicture.ts
+++ src/modules/user/ProfilePicture.ts
@@ -1,6 +1,7 @@
 import { Resolver, Mutation, Arg } from 'type-graphql';
 import { GraphQLUpload, FileUpload } from 'graphql-upload';
 import { createWriteStream } from 'fs';
+import { ROOT_DIR } from 'src/config/conf';
 
 // import { FileUpload } from '../../types/file-upload';
 
@@ -13,9 +14,10 @@ export class ProfilePictureResolver {
   ): Promise<boolean> {
     console.log('upload', upload);
     const { createReadStream, filename } = upload;
+
     return new Promise(async (resolve, reject) =>
       createReadStream()
-        .pipe(createWriteStream(__dirname + `/../../../images/${new Date().getTime()}_${filename}`))
+        .pipe(createWriteStream(`${ROOT_DIR}images/${new Date().getTime()}_${filename}`))
         .on('finish', () => resolve(true))
         .on('close', () => resolve(true))
         .on('error', () => reject(false))
